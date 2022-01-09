4a4f7c861eebd0e044ee547294eddb0c856711eb
Ravi-nanda-bala <ruderavi1998@gmail.com>
2022-01-09 20:46:52 +0530

    msg

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ExceltoJson from './ExcelReader.js'
 import reportWebVitals from './reportWebVitals';
+import Button from '@material-ui/core/Button';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+ 
+
+
+
+    <ExceltoJson />
+
+
+
+  
+  
+  
+  
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
